eb09e715395013d55a582cdb08d5abc40299d120
Luke9124 <157318621+Luke9124@users.noreply.github.com>
2024-05-27 10:29:36 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,20 +1,3 @@
-// Przed odejściem, programista popsuł kod źródłowy zarządzania kontami użytkowników naszego serwisu dostawy jedzenia.
-// Przeprowadź refaktoryzację metod obiektu customer, dodając brakujące this przy odwołaniach do właściwości obiektu.
-// Użyj tego kodu startowego i wykonaj refaktoryzację.Po deklaracji obiektu dodaliśmy wywołania metod.
-// W konsoli zostaną wyświetlone wyniki ich pracy.Proszę, nic tam nie zmieniaj.
-// Zadeklarowana zmienna customer
-// Wartość zmiennej customer to obiekt z właściwościami i metodami
-// Wywołanie customer.getDiscount() zwraca aktualną wartość właściwości discount
-// Wywołanie customer.setDiscount(0.15) aktualizuje wartość właściwości discount
-// Wywołanie customer.getBalance() zwraca aktualną wartość właściwości balance
-// Wywołanie customer.getOrders() zwraca aktualną wartość właściwości orders
-// Wywołanie customer.addOrder(5000, "Steak") dodaje "Steak" do tablicy wartości właściwości orders i aktualizuje saldo
-// Metoda getBalance obiektu customer używa this
-// Metoda getDiscount obiektu customer używa this
-// Metoda setDiscount obiektu customer używa this
-// Metoda getOrders obiektu customer używa this
-// Metoda addOrder obiektu customer używa this
-
 const customer = {
   username: 'Mango',
   balance: 24000,
