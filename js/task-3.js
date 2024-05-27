3f0ce70ab001e1934cb6dd2c514341591134bffe
Luke9124 <157318621+Luke9124@users.noreply.github.com>
2024-05-27 10:38:55 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,32 +1,25 @@
-// Napisz klasę StringBuilder, która przyjmuje jeden parametr initialValue — dowolny ciąg znaków,
-// który jest zapisywany w prywatnej właściwości value obiektu, który jest tworzony.
+class StringBuilder {
+    #value;
+    constructor( initialValue = "" ) {
+        this.#value = initialValue;
+    }
 
-// Ogłoś następujące metody klasy:
+    getValue(){
+        return this.#value;
+    }
 
-// getValue() — zwraca aktualną wartość prywatnej właściwości value.
-// padEnd(str) — otrzymuje parametr str (ciąg znaków) i dodaje go na końcu wartości prywatnej właściwości value obiektu, który wywołuje tę metodę.
-// padStart(str) — otrzymuje parametr str (ciąg znaków) i dodaje go na początku wartości prywatnej właściwości value obiektu, który wywołuje tę metodę.
-// padBoth(str) — otrzymuje parametr str (ciąg znaków) i dodaje go na początku i na końcu wartości prywatnej właściwości value obiektu, który wywołuje tę metodę.
+    padEnd(str){
+        this.#value += str;
+    }
 
-// Zadeklarowana klasa StringBuilder
-// Właściwość value w klasie StringBuilder zadeklarowana jako prywatna
-// W klasie StringBuilder zadeklarowana metoda getValue
-// Metoda getValue zwraca wartość prywatnej właściwości value instancji klasy, która ją wywołuje
-// W klasie StringBuilder zadeklarowana metoda padEnd
-// Metoda padEnd zmienia wartość prywatnej właściwości value instancji klasy, która ją wywołuje
-// W klasie StringBuilder zadeklarowana metoda padStart
-// Metoda padStart zmienia prywatną właściwość value instancji klasy, która ją wywołuje
-// W klasie StringBuilder zadeklarowana metoda padBoth
-// Metoda padBoth zmienia wartość prywatnej właściwości value instancji klasy, która ją wywołuje
-// W wyniku wywołania new StringBuilder(".") wartość prywatnej zmiennej builder to obiekt
-// Obiekt builder nie zawiera publicznej właściwości value
-// Pierwsze wywołanie builder.getValue() zaraz po inicjalizacji instancji zwraca ciąg znaków .
-// Drugie wywołanie builder.getValue() po wywołaniu builder.padStart("^") zwraca ciąg znaków ^.
-// Trzecie wywołanie builder.getValue() po wywołaniu builder.padEnd("^") zwraca ciąg znaków ^.^
-//     Czwarte wywołanie builder.getValue() po wywołaniu builder.padBoth("=") zwraca ciąg znaków =^.^=
+    padStart(str){
+        this.#value = str + this.#value;
+    }
 
-// Weź kod poniżej z inicjalizacją instancji i wywołaniami metod i wstaw go po deklaracji klasy,
-// aby sprawdzić poprawność działania.W konsoli zostaną wyświetlone wyniki ich pracy.Proszę, nic tam nie zmieniaj.
+    padBoth(str){
+        this.#value = str + this.#value + str;
+    }
+}
 
 const builder = new StringBuilder('.');
 console.log(builder.getValue()); // "."
